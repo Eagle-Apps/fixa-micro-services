2aefb606c0016efbebae3be58c7f17a693428d33
Obed Okpala <katlybedrick@gmail.com>
2022-09-25 18:39:49 +0100

    fix stash issues

--- services/fault-service-management/src/api/faultService.js
+++ services/fault-service-management/src/api/faultService.js
@@ -9,33 +9,6 @@ import {
 export const fault = (app) => {
   const service = new FaultService();
 
-<<<<<<< Updated upstream
-  // app.post("/request-variable-service", async (req, res, next) => {
-  //   const { description, schedule, serviceCategory } = req.body;
-
-  //   const { userId } = req.user;
-
-  //   try {
-  //     //create new service request
-  //     const { data } = await service.AddServiceRequest({
-  //       userId,
-  //       description,
-  //       schedule,
-  //     });
-
-  //     // handle all notifications
-
-  //     const payload = { event: "VARIABLE_SERVICE", data };
-  //     PublishNotificationEvent(payload);
-
-  //     return res.json({
-  //       message: "request created, assigning tecnician in progress",
-  //     });
-  //   } catch (err) {
-  //     next(err);
-  //   }
-  // });
-=======
   app.get("/fetchtasks", async (req, res, next) => {
     try {
       const { data } = await service.GetAllRequests();
@@ -99,5 +72,4 @@ export const fault = (app) => {
       next(err);
     }
   });
->>>>>>> Stashed changes
 };
