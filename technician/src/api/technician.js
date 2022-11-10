34d20efa300865cc52c8228d33a99433550c789d
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-11-10 11:14:11 +0100

    update

--- technician/src/api/technician.js
+++ technician/src/api/technician.js
@@ -161,17 +161,17 @@ export const technician = (app, channel) => {
         zipCode,
       });
 
-      const payload = {
-        event: "SIGN_UP",
-        data,
-      };
+      // const payload = {
+      //   event: "SIGN_UP",
+      //   data,
+      // };
 
-      PublishNotificationEvent(payload);
-      PublishMessage(
-        this.channel,
-        NOTIFICATION_SERVICE,
-        JSON.stringify(payload)
-      );
+      // PublishNotificationEvent(payload);
+      // PublishMessage(
+      //   this.channel,
+      //   NOTIFICATION_SERVICE,
+      //   JSON.stringify(payload)
+      // );
 
       return res.json({"message" : "sucessfully signed up", data } );
 
