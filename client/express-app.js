4d96d4fcd59b99757f0d15f98967e74c943a93d7
Obed Okpala <katlybedrick@gmail.com>
2022-12-05 15:26:44 +0100

    fix cors issues

--- client/express-app.js
+++ client/express-app.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
 import { client } from "./src/api/client.js";
-import { appEvents } from "./src/api/app-events.js";
 import HandleErrors from "./src/utils/error-handler.js";
 
 export const expressApp = async (app, channel) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
+  app.use(cors({ origin: "*" }));
 
   //api
   client(app, channel);
