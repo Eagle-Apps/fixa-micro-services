2c8f01d4636f9a9fa0b73fb558dc8baa5045bac6
Kelvin Kelechi Ibeh <81246393+papikelscode@users.noreply.github.com>
2023-07-06 04:55:32 -0700

    text case changed

--- staff/src/dba/models/staff.js
+++ staff/src/dba/models/staff.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const staffSchema = mongoose.Schema(
   {
-    Firstname: {
+    FirstName: {
       type: String,
     },
-    Lastname: {
+    LastName: {
       type: String,
     },
     Email: {
